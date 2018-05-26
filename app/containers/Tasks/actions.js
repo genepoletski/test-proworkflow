84cacb594251d7c5a614aa908c9a66e29be15925
eugene <web.developer.poletski@gmail.com>
2018-05-26 16:24:45 +1200

    Add action creators to Tasks

---
+++ app/containers/Tasks/actions.js
@@ -0,0 +1,35 @@
+import {
+  ACTION_TASKS_FIND_TASK_LIST_REQUESTED,
+  ACTION_TASKS_FIND_TASK_LIST_SUCCEEDED,
+  ACTION_TASKS_FIND_TASK_LIST_FAILED,
+} from './actionTypes';
+
+export function actionTasksFindTaskListRequest() {
+  return {
+    type: ACTION_TASKS_FIND_TASK_LIST_REQUESTED,
+    payload: null,
+  };
+}
+
+export function actionTasksFindTaskListSucceed({
+  count,
+  tasks,
+  totalcount,
+}) {
+  return {
+    type: ACTION_TASKS_FIND_TASK_LIST_SUCCEEDED,
+    payload: {
+      count,
+      tasks,
+      totalcount,
+    },
+  };
+}
+
+export function actionTasksFindTaskListFail({ message }) {
+  return {
+    type: ACTION_TASKS_FIND_TASK_LIST_FAILED,
+    payload: new Error(message),
+    error: true,
+  };
+}
