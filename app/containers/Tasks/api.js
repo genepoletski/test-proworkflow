b1d02b3949adb6cbcd14295b91dc181c9419dc5a
eugene <web.developer.poletski@gmail.com>
2018-05-27 11:30:09 +1200

    Fix url query string concatenation in Tasks service api

--- app/containers/Tasks/api.js
+++ app/containers/Tasks/api.js
@@ -1,18 +1,26 @@
 import 'whatwg-fetch';
 
+const restAPIbaseURL = 'https://api.proworkflow.net';
+
+
 export async function apiFindTaskList() {
-  const url = 'https://api.proworkflow.net/tasks?fields=name,project,projectcategory,timeallocated,timetracked,completedate';
+  try {
+    const query = 'fields=name,project,projectcategory,timeallocated,timetracked,completedate';
+    const url = (`${restAPIbaseURL}/tasks?${query}`);
 
-  const requestConfig = {
-    method: 'GET',
-    headers: {
-      Authorization: 'Basic ZzFrSmZTazczaGo6dUg4M2s5cVc1MTNiY3pEMTg=',
-      apikey: 'VE4J-DTOL-GB9N-GPB9-PWFISMQ-DEV3572',
-    },
-  };
+    const requestConfig = {
+      method: 'GET',
+      headers: {
+        Authorization: 'Basic ZzFrSmZTazczaGo6dUg4M2s5cVc1MTNiY3pEMTg=',
+        apikey: 'VE4J-DTOL-GB9N-GPB9-PWFISMQ-DEV3572',
+      },
+    };
 
-  const response = await fetch(url, requestConfig);
-  const json = response.toJSON();
+    const response = await fetch(url, requestConfig);
+    const json = response.json();
 
-  return json;
+    return json;
+  } catch (err) {
+    throw err;
+  }
 }
